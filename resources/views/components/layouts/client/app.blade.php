523bc7f3fefbad16125ecdd7e82969ec7c6b0559
bakliounsaid <saidb@fb-technologies.com>
2025-01-05 22:19:44 +0100

    sem

--- resources/views/components/layouts/client/app.blade.php
+++ resources/views/components/layouts/client/app.blade.php
@@ -4,8 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Votre entreprise de décoration et installation de rideaux, et réseaux.">
+    <meta name="keywords" content="rideaux, décoration intérieure, installation, réseaux">
+    <meta name="author" content="Chaima Rideaux">
     <title>Chaima Rideaux</title>
+
+    <!-- Open Graph (pour les réseaux sociaux) -->
+    <meta property="og:title" content="Page d'accueil - Décoration de Fenêtres">
+    <meta property="og:description" content="Découvrez notre gamme de produits pour fenêtres, rideaux, et décoration.">
+    <meta property="og:image" content="{{ asset('assets/img/photos/logo.jpg') }}">
+    <meta property="og:url" content="{{ url()->current() }}">
+
+    <!-- Twitter Card (pour Twitter) -->
+    <meta name="twitter:title" content="Page d'accueil - Décoration de Fenêtres">
+    <meta name="twitter:description" content="Découvrez notre gamme de produits pour fenêtres, rideaux, et décoration.">
+    <meta name="twitter:image" content="{{ asset('assets/img/photos/logo.jpg') }}">
+    <meta name="twitter:card" content="summary_large_image">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="shortcut icon" href="{{ asset('assets/img/photos/logo.jpg') }}" />
 
